af4f851a0d3c65e870db8dacf03052d7701087d9
HADAS213 <HADAsmar014@gmail.com>
2023-06-11 22:29:32 +0300

    up

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const {config} = require('../config/secret');
 
 main().catch(err => console.log(err));
-
 async function main() {
   await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.jbkxtua.mongodb.net/black23`);
   console.log("connect")
